ff796f5066b4e16be07fa281e81cad700cde3039
Karanbir Singh <kbsingh@karan.org>
2019-10-31 13:49:18 +0000

    try a 8 build for cico

--- vagrant/do_vagrant_cbs.sh
+++ vagrant/do_vagrant_cbs.sh
@@ -2,6 +2,7 @@
 # To see all options in koji -p command check "koji -p cbs image-build --help"
 set -eu
 
+
 usage()
 {
   cat << EOF
@@ -9,11 +10,30 @@ usage: $(basename $0) <argument>
   where <argument> is one of:
     6   -- build Vagrant images for CentOS 6
     7   -- build Vagrant images for CentOS 7
-    all -- build Vagrant images for both CentOS 6 and 7
+    8   -- build Vagrant images for CentOS 8
+    all -- build Vagrant images for both CentOS 6 and 7 and 8
 EOF
   exit 1
 }
 
+
+build_vagrant_image_8()
+{
+  BOOT_REPO="http://mirror.centos.org/centos/8/BaseOS/x86_64/os/"
+  INSTALL_REPO=" --repo http://mirror.centos.org/centos/8/AppStream/x86_64/os/ \
+  --repo http://mirror.centos.org/centos/8/extras/x86_64/os/ \
+  --repo http://mirror.centos.org/centos/8/PowerTools/x86_64/os/"
+  build_vagrant_image 8
+}
+
+build_vagrant_image_67()
+{
+  EL_MAJOR=$1
+  BOOT_REPO="http://mirror.centos.org/centos/${EL_MAJOR}/os/x86_64/"
+  INSTALL_REPO=" --repo http://mirror.centos.org/centos/${EL_MAJOR}/extras/x86_64/ \
+  --repo http://mirror.centos.org/centos/${EL_MAJOR}/updates/x86_64/ "
+  build_vagrant_image $EL_MAJOR
+}
 
 build_vagrant_image()
 {
@@ -26,7 +46,7 @@ build_vagrant_image()
   EL_MAJOR=$1
   koji -p cbs image-build \
     centos-${EL_MAJOR} 1  cloudinstance${EL_MAJOR}-common-el${EL_MAJOR} \
-    http://mirror.centos.org/centos/${EL_MAJOR}/os/x86_64/ x86_64 \
+    ${BOOT_REPO}  x86_64 \
     --release=1 \
     --distro RHEL-${EL_MAJOR}.0 \
     --ksver RHEL${EL_MAJOR} \
@@ -37,8 +57,7 @@ build_vagrant_image()
     --format=vagrant-hyperv \
     --factory-parameter fusion_scsi_controller_type pvscsi \
     --ova-option vagrant_sync_directory=/vagrant \
-    --repo http://mirror.centos.org/centos/${EL_MAJOR}/extras/x86_64/\
-    --repo http://mirror.centos.org/centos/${EL_MAJOR}/updates/x86_64/\
+    ${INSTALL_REPO} \
     --scratch \
     ${WAIT:-"--nowait"} \
     --disk-size=40
@@ -51,14 +70,18 @@ fi
 
 case $1 in
   6)
-    build_vagrant_image 6
+    build_vagrant_image_67 6
     ;;
   7)
-    build_vagrant_image 7
+    build_vagrant_image_67 7
+    ;;
+  8)
+    build_vagrant_image_8
     ;;
   all)
-    build_vagrant_image 6
-    build_vagrant_image 7
+    build_vagrant_image_67 6
+    build_vagrant_image_67 7
+    build_vagrant_image_8
     ;;
   *)
     usage
